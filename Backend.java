39bf996a73c2e8c9b77720508b64a9d993aa74cd
Muhammad Hendry Prasetya <45161697+hendpraz@users.noreply.github.com>
2019-04-15 14:30:36 +0700

    Create Backend.java

---
+++ Backend.java
@@ -0,0 +1,17 @@
+public class Backend{
+	public static float KMP(Object s1, Object s2){
+		// Algoritma Knuth-Morris-Prath
+	}
+	
+	public static float Regex(Object s1, Object s2){
+		// Algoritma Regular Expression
+	}
+	
+	public static float BM(Object s1, Object s2){
+		// Algoritma Boyer-Moore
+	}
+	
+	public static float otherFunc(Object s1, Object s2){
+		// Fungsi buatan sendiri
+	}
+}
