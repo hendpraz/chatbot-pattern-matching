c6fa0709139be485dc6fee51506a09ba65d303d1
Muhammad Hendry Prasetya <45161697+hendpraz@users.noreply.github.com>
2019-04-18 00:14:48 +0700

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -10,8 +10,48 @@ answerDB = []
 factory = StopWordRemoverFactory()
 stopwords = factory.get_stop_words()
 
-def knuthMorrisPratt(str1, str2):
-    return 90
+def borderFunctionKMP(str, m):
+    suffLen = 0
+
+    border = [0]*m
+    i = 1
+
+    while (i < m):
+        if (str[i] == str[suffLen]):
+            suffLen = suffLen + 1
+            border[i] = suffLen
+            i = i + 1
+        else:
+            if (suffLen != 0):
+                suffLen = border[suffLen - 1]
+            else:
+                border[i] = 0
+                i = i + 1
+
+    return border
+
+def knuthMorrisPratt(string1, txt):
+    tokenizedString = word_tokenize(string1)
+    countMatch = 0
+    totalLength = len(txt) - 1 #Dikurang tanda tanya
+    for pattern in tokenizedString:
+        m = len(pattern)
+        n = len(txt) - 1
+
+        border = borderFunctionKMP(pattern,m)
+        i = 0
+        while (i < n):
+            if (pattern[j] == txt[i]):
+                i = i + 1
+                j = j + 1
+
+            if (j == m):
+                #Pattern ditemukan
+                countMatch = countMatch + m + 1 #Ditambah sebuah spasi
+            elif(i < n):
+                #Tidak cocok, geser
+                if()
+    return (countMatch * 1.0 / totalLength)
 
 def maxKMP(string):
     #knuth-morris-pratt
@@ -68,13 +108,18 @@ def removeStopWords(string):
     wordTokens = word_tokenize(string)
     for w in wordTokens
         if not w in stopwords
-            filteredString = filteredString + w
+            filteredString = filteredString + " " + w
 
 # Main program #
 initDB()
 string = str(input())
 string = removeStopWords(string)
+# DEBUG:
+print("Filetered string:")
+print(string)
 max, idx1 = maxKMP(string)
+print("max = " + str(max))
+print("idx = " + str(idx))
 if(max >= 90):
     print(answerDB[idx])
     print("Answered with Knuth-Morris-Pratt")
