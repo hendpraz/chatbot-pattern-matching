02d4ba6c31964db7abc56039c1c5d7db9116dae9
ChristzenLeonardy <christzenleonardy@gmail.com>
2019-04-22 02:12:28 +0700

    Front-end done

--- index.php
+++ index.php
@@ -1,94 +1,48 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <style>
-            body {
-                /* margin: 0 auto; */
-                background-color: teal;
-                max-width: 800px;
-                padding: 0 20px;
-                font-family: Arial;
-            }
-
-            .chatbox {
-                background-color: yellowgreen;
-                float: left;
-                width: 1000px;
-                overflow-y: auto;
-                height: 400px;
-                padding: 0 20px;
-                margin: 20px auto;
-                border-radius: 5px;
-            }
-
-            .container {
-                border: 2px solid #dedede;
-                background-color: #f1f1f1;
-                border-radius: 5px;
-                padding: 10px;
-                margin: 10px 0;
-            }
-
-            .darker {
-                border-color: #ccc;
-                background-color: #ddd;
-            }
-
-            .container::after {
-                content: "";
-                clear: both;
-                display: table;
-            }
-
-            .container img {
-                float: left;
-                max-width: 60px;
-                width: 100%;
-                margin-right: 20px;
-                border-radius: 50%;
-            }
-
-            .container img.right {
-                float: right;
-                margin-left: 20px;
-                margin-right:0;
-            }
-
-            .container p.right {
-                text-align: right;
-            }
-        </style>
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="./images/snowyOwl01.png" type="image/x-icon"/>
+        <title>Chat with Fluffball</title>
     </head>
     <body>
-        <h2>Chat Messages</h2>
+        <h2 style = "color: white"><b>Chat with Fluffball</b></h2>
 
-        <div class="chatbox">
+        <div class="chatbox" id="MainChatBox">
             <?php
-                $chatLogFile = fopen("chatLog.txt", "r+");
+                $chatLogFile = fopen("chatLog.txt", "r");
                 $chatLog = array();
                 $shade = true;
                 $i = 0;
-                //Tampilkan chat yang telah ada
+
+                // Tampilkan chat yang telah ada
                 while (!feof($chatLogFile)) {
                     if ($shade) {
                         array_push($chatLog, fgets($chatLogFile));
-                        echo
-                            "<div class=\"container\">
-                                <img src=\"./images/bot.png\" alt=\"Bot\" style=\"width:100%;\">
-                                <p>" . $chatLog[$i] . "</p>
-                            </div>";
+                        ?>
+
+                            <div class = "container darker">
+                                <img src = "./images/SnowyOwlEye.gif" alt = "Bot">
+                                <p> <?= $chatLog[$i]?> </p>
+                            </div>
+
+                        <?php
                         $i = $i + 1;
                     }
                     else {
                         array_push($chatLog, fgets($chatLogFile));
-                        echo
-                            "<div class=\"container darker\">
-                                <img src=\"./images/user.png\" alt=\"User\" class =\"right\" style=\"width:100%;\">
-                                <p class =\"right\">" . $chatLog[$i] . "</p>
-                            </div>";
-                            $i = $i + 1;
+                        ?>
+
+                            <div class = "container">
+                                <img src = "./images/user.png" alt = "User" class = "right">
+                                <p class = "right"> <?= $chatLog[$i]?> </p>
+                            </div>
+
+                        <?php
+                        $i = $i + 1;
                     }
                     $shade = !$shade;
                 }
@@ -96,21 +50,24 @@
                 //Tampilkan pertanyaan pertama
                 $choicesFile = fopen("choices.txt","r");
                 while (!feof($choicesFile)){
-                  //Ambil choices terakhir
-                  $choice = intval(fgets($choicesFile));
+                    //Ambil choices terakhir
+                    $choice = intval(fgets($choicesFile));
                 }
                 fclose($choicesFile);
 
                 //Cek apakah choice == -1
                 if($choice == -1){
-                  $output = "Halo! Panduan menggunakan: Masukkan angka 1 s.d. 3 untuk menggunakan algoritma 1: KMP, 2:BM, 3: Regex. Untuk mengganti algoritma yang digunakan, tulis 'ganti'";
-                  array_push($chatLog, $output);
-                  echo
-                      "<div class=\"container\">
-                          <img src=\"./images/bot.png\" alt=\"Bot\" style=\"width:100%;\">
-                          <p>" . $output . "</p>
-                      </div>";
-                      fwrite($chatLogFile, "\n".trim($output));
+                    $output = "Halo! Panduan menggunakan: Masukkan angka 1 s.d. 3 untuk menggunakan algoritma 1: KMP, 2:BM, 3: Regex. Untuk mengganti algoritma yang digunakan, tulis 'ganti'";
+                    array_push($chatLog, $output);
+                    ?>
+
+                        <div class = "container darker">
+                            <img src = "./images/SnowyOwlEye.gif" alt = "Bot">
+                            <p> <?= $output?> </p>
+                        </div>
+
+                    <?php
+                    fwrite($chatLogFile, "\n".trim($output));
                 }
 
                 fclose($chatLogFile);
@@ -118,91 +75,118 @@
                 if (isset($_POST['chat'])) {
                     //Tulis ulang semua percakapan ke chatlog
                     $chatLogFile = fopen("chatLog.txt", "w");
+
                     $chat = $_POST['chat'];
                     for($num = 0; $num < count($chatLog); $num++)
                         fwrite($chatLogFile, $chatLog[$num]);
                     fwrite($chatLogFile, "\n".trim($chat));
 
                     //Tampilkan chat dari user
-                    echo
-                            "<div class=\"container darker\">
-                                <img src=\"./images/user.png\" alt=\"User\" class =\"right\" style=\"width:100%;\">
-                                <p class =\"right\">" . $chat . "</p>
-                            </div>";
+                    ?>
 
-					//Cek apakah pilihan algoritma sudah ada atau belum
+                        <div class = "container">
+                            <img src = "./images/user.png" alt = "User" class = "right">
+                            <p class = "right"> <?= $chat?> </p>
+                        </div>
+
+                    <?php
+
+                    //Cek apakah pilihan algoritma sudah ada atau belum
                     if(($choice == 0) || ($choice == -1)){
                         if((intval($chat) >= 1) && (intval($chat) <= 3)){
                             $choice = intval($chat);
                             if($choice == 1){
-                              $output = "Silahkan tanyakan apa saja! *KMP*";
+                                $output = "Silahkan tanyakan apa saja! *KMP*";
                             } else if($choice == 2){
-                              $output = "Silahkan tanyakan apa saja! *BM*";
+                                $output = "Silahkan tanyakan apa saja! *BM*";
                             } else{ //choice == 3
-                              $output = "Silahkan tanyakan apa saja! *Regex*";
+                                $output = "Silahkan tanyakan apa saja! *Regex*";
                             }
                             $choicesFile = fopen("choices.txt","w");
                             fwrite($choicesFile, $choice);
                             fclose($choicesFile);
 
                             array_push($chatLog, $output);
-                            echo
-                                "<div class=\"container\">
-                                    <img src=\"./images/bot.png\" alt=\"Bot\" style=\"width:100%;\">
-                                    <p>" . $output . "</p>
-                                </div>";
+
+                            ?>
+
+                                <div class = "container darker">
+                                    <img src = "./images/SnowyOwlTalk.gif" alt = "Bot">
+                                    <p> <?= $output?> </p>
+                                </div>
+
+                            <?php
+
                         } else{
-                          $output = "Input salah, masukkan angka 1 s.d. 3. 1: KMP, 2:BM, 3: Regex";
-                          array_push($chatLog, $output);
-                          echo
-                              "<div class=\"container\">
-                                  <img src=\"./images/bot.png\" alt=\"Bot\" style=\"width:100%;\">
-                                  <p>" . $output . "</p>
-                              </div>";
+                            $output = "Input salah, masukkan angka 1 s.d. 3. 1: KMP, 2:BM, 3: Regex";
+                            array_push($chatLog, $output);
+                            ?>
+
+                                <div class = "container darker">
+                                    <img src = "./images/SnowyOwlTalk.gif" alt = "Bot">
+                                    <p> <?= $output?> </p>
+                                </div>
+
+                            <?php
                         }
                         fwrite($chatLogFile, "\n".trim($output));
                     } else{
-                      if($chat == "ganti"){
-						//ganti dengan algoritma lain
-                        $choicesFile = fopen("choices.txt","w");
-                        $output = "Masukkan angka 1 s.d. 3 untuk menggunakan algoritma 1: KMP, 2:BM, 3: Regex. Untuk mengganti algoritma yang digunakan, tulis 'ganti'";
-                      //array_push($chatLog, $output);
-                        echo
-                            "<div class=\"container\">
-                                <img src=\"./images/bot.png\" alt=\"Bot\" style=\"width:100%;\">
-                                <p>" . $output . "</p>
-                            </div>";
-                            fwrite($chatLogFile, "\n".trim($output));
-                        fwrite($choicesFile, "0");
-                        fclose($choicesFile);
-                      } else{
+                        if($chat == "ganti"){
+                            //ganti dengan algoritma lain
+                            $choicesFile = fopen("choices.txt","w");
+                            $output = "Masukkan angka 1 s.d. 3 untuk menggunakan algoritma 1: KMP, 2:BM, 3: Regex. Untuk mengganti algoritma yang digunakan, tulis 'ganti'";
+                            //array_push($chatLog, $output);
+                            ?>
 
-                        //Lakukan pattern matching sesuai $choice
-                        //Debug
-                        $cmd = "py Backend.py " . $choice;
-                        $output = shell_exec($cmd);
-                        //array_push($chatLog, $output);
-                        echo
-                            "<div class=\"container\">
-                                <img src=\"./images/bot.png\" alt=\"Bot\" style=\"width:100%;\">
-                                <p>" . $output . "</p>
-                            </div>";
-                        fwrite($chatLogFile, "\n".trim($output));
-                      }
+                                <div class = "container darker">
+                                    <img src = "./images/SnowyOwlTalk.gif" alt = "Bot">
+                                    <p> <?= $output?> </p>
+                                </div>
+
+                            <?php
+                            fwrite($chatLogFile, "\n".trim($output));
+                            fwrite($choicesFile, "0");
+                            fclose($choicesFile);
+                            } else{
+
+                            //Lakukan pattern matching sesuai $choice
+                            //Debug
+                            $cmd = "py Backend.py " . $choice;
+                            $output = shell_exec($cmd);
+                            //array_push($chatLog, $output);
+                            ?>
+
+                                <div class = "container darker">
+                                    <img src = "./images/SnowyOwlTalk.gif" alt = "Bot">
+                                    <p> <?= $output?> </p>
+                                </div>
+
+                            <?php
+                            fwrite($chatLogFile, "\n".trim($output));
+                        }
                     }
-                  fclose($chatLogFile);
+
+                    fclose($chatLogFile);
                 }
             ?>
+            <script>
+                function scrollDownChat() {
+                    var elmnt = document.getElementById("MainChatBox");
+                    elmnt.scrollTop = elmt.scrollHeight;
+                }
+            </script>
         </div>
 
-        <form action="index.php" method="POST" style = "margin-left = 20px">
-            <table border="0">
-                <tr>
-                    <td><input type="text" name="chat" size="30" /></td>
-                    <td><input type="submit" value="Submit"/></td>
-                </tr>
-            </table>
-        </form>
+        <div onLoad="scrollDownChat();">
+            <form action="index.php" method="POST" style = "margin-left = 200px">
+                <table border="0">
+                    <tr>
+                        <td><input type="text" name="chat" size="100" /></td>
+                        <td><input type="submit" value="Send"/></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
 
     </body>
 </html>
